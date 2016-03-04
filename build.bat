f5ed939ea110f28133a8c3908de7b451bf26dd19
Noel O'Boyle <baoilleach@gmail.com>
2016-03-04 16:19:36 +0000

    Add my build script just in case someone finds it useful

---
+++ build.bat
@@ -0,0 +1,2 @@
+cmake .. -DOPENBABEL_INCLUDE_DIR=C:\Tools\openbabel\baoilleach\include -DOPENBABEL_INCLUDE_DIR2=C:\Tools\openbabel\baoilleach\build\include -G "Visual Studio 11" -DOPENBABEL_LIBRARIES=C:\Tools\openbabel\baoilleach\build\src\Debug\openbabel-2.lib
+
